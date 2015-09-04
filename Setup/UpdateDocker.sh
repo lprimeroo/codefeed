9decbd0e53ca3faeac95e84e96688a2c41b35830
Sarthak Munshi <sarthakmunshi@gmail.com>
2015-09-05 00:42:09 +0530

    new branch

--- Setup/UpdateDocker.sh
+++ Setup/UpdateDocker.sh
@@ -1,5 +1,5 @@
 
 echo "Creating Docker Image"
-docker build -t 'virtual_machine_codefeed' - < Dockerfile
+docker build -t 'virtual_machine' - < Dockerfile
 echo "Retrieving Installed Docker Images"
 docker images
